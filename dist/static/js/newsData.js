3c5d68f23e33d33de2d308d87157fbef3bcfb38b
Tianyou Song <songtianyou07@gmail.com>
2018-08-03 17:17:27 +0800

    init

---
+++ dist/static/js/newsData.js
@@ -0,0 +1,76 @@
+export default function getData () {
+  return [
+    {
+      imgUrl: '../../static/img/news1.jpg',
+      title: 'PWR BTTM pens letter addressing sexual assault allegations',
+      tags: ['Music', 'News'],
+      time: '10 hours ago',
+      desc: 'The duo released a new statement after their music was pulled from streaming services',
+      width: 0
+    },
+    {
+      imgUrl: '../../static/img/news2.jpg',
+      title: 'Hitting an LA pool hall in No Vacancy Inn’s new collection',
+      tags: ['Culture'],
+      time: '12 hours ago',
+      desc: 'The third collection from the Dazed 100 duo pulls inspiration from Truman Capote and Edward Norton – see it here',
+      width: 0
+    },
+    {
+      imgUrl: '../../static/img/news3.jpg',
+      title: 'The male romper is the must haven’t item for the summer',
+      tags: ['Fashion'],
+      time: '12 hours ago',
+      desc: 'Stop trying to make male rompers happen, it’s not going to happen. Oh my god, what if it happens',
+      width: 0
+    },
+    {
+      imgUrl: '../../static/img/news4.jpg',
+      title: 'The Dark Crystal is getting a Netflix prequel series',
+      tags: ['Music'],
+      time: '14 hours ago',
+      desc: 'Jim Henson’s cult creature feature will return, many years before the original adventure',
+      width: 0
+    },
+    {
+      imgUrl: '../../static/img/news5.jpg',
+      title: 'Gay Chechens fleeing abuse are being denied U.S visas',
+      tags: ['Culture'],
+      time: '12 hours ago',
+      desc: 'Approximately 40 survivors, hiding throughout Russia, are having difficulty finding safety',
+      width: 0
+    },
+    {
+      imgUrl: '../../static/img/news6.jpg',
+      title: 'Watch a weird Black Mirror & Orange is the New Black mash-up',
+      tags: ['Arts'],
+      time: '18 hours ago',
+      desc: 'Taystee and Poussey reunite in this one-off recreation of ‘San Junipero’',
+      width: 0
+    },
+    {
+      imgUrl: '../../static/img/news7.jpg',
+      title: 'See through Sampha’s eyes with his new zine',
+      tags: ['Music'],
+      time: '12 hours ago',
+      desc: 'The singer partners with Grace Wales Bonner and Dazed’s Jamie Andrew Reid to dive into his creative and personal roots in London and Sierra Leone',
+      width: 0
+    },
+    {
+      imgUrl: '../../static/img/news8.gif',
+      title: 'Here’s why young people aren’t voting',
+      tags: ['Fashion'],
+      time: '12 hours ago',
+      desc: 'The establishment loves to accuse 18-24s of being apathetic – but with the way the system is designed and how it’s discussed, who’s surprised?',
+      width: 0
+    },
+    {
+      imgUrl: '../../static/img/news9.jpg',
+      title: 'Protesters attacked outside Turkish Embassy in Washington',
+      tags: ['Arts'],
+      time: '16 hours ago',
+      desc: "Shocking footage shows President Erdoğan's security guards kicking demonstrators in the face",
+      width: 0
+    }
+  ]
+}
